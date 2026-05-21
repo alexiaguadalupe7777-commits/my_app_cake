678b5d02f08629137006bf3bf6bdc4abd490e34c
Alexia <alexiaguadalupe7777@gmail.com>
2026-05-21 13:13:07 -0600

    fix: select explicito y created null

--- templates/Articles/index.php
+++ templates/Articles/index.php
@@ -2,6 +2,17 @@
 
 <?php foreach ($articles as $article): ?>
     <article>
-        <h2><?= var_dump($article->toArray()) ?></h2>
+        <h2>
+            <?= $this->Html->link(
+                h($article->title),
+                ['action' => 'view', $article->slug]
+            ) ?>
+        </h2>
+        <p>
+            <small>Published: <?= $article->created ? $article->created->format('F d, Y') : 'N/A' ?></small>
+        </p>
+        <p><?= h($article->body) ?></p>
     </article>
 <?php endforeach; ?>
+
+<?= $this->Html->link('New Article', ['action' => 'add'], ['class' => 'button']) ?>
